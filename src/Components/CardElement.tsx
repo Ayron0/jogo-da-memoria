a301d6dad47155fe94a4f38318478aa29619386c
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-05-01 13:38:17 -0300

    FIX: props correction

--- src/Components/CardElement.tsx
+++ src/Components/CardElement.tsx
@@ -1,4 +1,15 @@
-export function CardElement(props) {
+type Card = {
+    id: string;
+    icon: string;
+    flipped: boolean;
+}
+
+interface CardElementProps {
+    card: Card;
+    handleFlip: (card: Card) => void;
+}
+
+export function CardElement(props: CardElementProps) {
     return(
         <div onClick={()=> {props.handleFlip(props.card)}} id={props.card.id} className={`card ${props.card.flipped?"flip":""}`}>
             <div className="card_front">
